5a4a488f624d446f053dba9104a59e9d22d41814
Carla Holcomb <carla.programmer@gmail.com>
2020-05-13 16:20:16 +0000

    updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
  
   root 'sessions#welcome'
+  
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create', as: 'session'
+  get '/signup', to: 'users#new', as: 'signup'
+  
   resources :cities
   resources :festivals
   resources :reviews
