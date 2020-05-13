def7e5a6dfcd861125052b0eb4c92a27c6b90914
Carla Holcomb <carla.programmer@gmail.com>
2020-05-13 16:34:01 +0000

    updated users controller with user params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,5 +17,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :username,
+      :password
+    )
+  end
 
 end
